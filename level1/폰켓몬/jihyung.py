ce558d15ee4e5bf25daa35eb3452bf95ee3159ee
wlgud0402 <wlgudrlgus@naver.com>
2022-01-24 23:38:50 +0900

    폰켓몬 -improved

--- level1/폰켓몬/jihyung.py
+++ level1/폰켓몬/jihyung.py
@@ -7,4 +7,11 @@ def solution(nums):
     return len(removed_double)
 
 
+# min함수를 사용한 if문 제거 가능
+def solution2(nums):
+    pick_count = len(nums)//2
+    removed_double = list(set(nums))
+    return min(pick_count, len(removed_double))
+
+
 print(solution([3, 3, 3, 2, 2, 4]))  # 3
