df76f000a8e2b90b8039cd7af3832ce7d21af98c
kim_jihyung <kim_jihyung@kim-jihyung.local>
2022-02-10 20:13:52 +0900

    modify:폴더구조변경

--- level1/3진법 뒤집기/jihyung.py
+++
@@ -1,6 +0,0 @@
-def solution(n):
-    pass
-
-
-print(solution(45))  # 7
-# print(solution(125))  # 229
--- level1/없는 숫자 더하기/jihyung.py
+++
@@ -1,6 +0,0 @@
-def solution(numbers):
-    a = set([i for i in range(10)])
-    return sum(list(a - set(numbers)))
-
-
-print(solution([1, 2, 3, 4, 6, 7, 8, 0]))  # 14
