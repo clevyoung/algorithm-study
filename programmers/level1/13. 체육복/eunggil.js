108e1e1509eb8591cdd0b024f8c70c982cf83dc5
김응길 <yesroad.dev@gmail.com>
2022-02-17 19:57:39 +0900

    체육복 - fail

--- programmers/level1/13. 체육복/eunggil.js
+++ programmers/level1/13. 체육복/eunggil.js
@@ -1,31 +1,25 @@
 function solution(n, lost, reserve) {
-  let answer = n - lost.length;
-  let stuArr = [];
-  let resArr = [];
-  
-  for (i in lost){
-      if(reserve.includes(lost[i])){
-          answer++
-      } else {
-          stuArr.push(lost[i] - 1)
-      }
-  }
-  
-  for(i in stuArr) { 
-      if (reserve.includes(stuArr[i])) {
-          answer ++
-      } else{
-          resArr.push(stuArr[i] + 1)
-      }
-  }
-  
-  for(i in resArr) { 
-      if (reserve.includes(resArr[i])) {
-          answer ++
-      } 
-  }    
-  
-  return answer;
+    let answer = n - lost.length;
+    let stuArr = [];
+    
+    for (i in lost){
+        if(reserve.includes(lost[i])){
+            answer++
+        } else {
+            stuArr.push(lost[i] - 1)
+        }
+    }
+    
+    for(i in stuArr) { 
+        if (reserve.includes(stuArr[i])) {
+            answer ++
+        } else if(reserve.includes(stuArr[i])){
+            answer ++
+        }
+    }
+    
+    
+    return answer;
 }
 
 solution(5,[2, 4],[1, 3, 5]) // solved
