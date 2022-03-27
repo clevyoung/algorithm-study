409d98d46b1bb71993a9a01ffada430f5e6e721d
김응길 <yesroad.dev@gmail.com>
2022-03-27 15:57:49 +0900

    완벽한 문자열 - solved

---
+++ programmers/test/test04/eunggil.js
@@ -0,0 +1,15 @@
+function solution(sentence) {
+	let answer = ''
+	const str = sentence.toLowerCase().replace(/[^a-z]/gi, '').split('')
+	const alphabet = []
+	
+	for(let i = 97; i <= 122; i++) {
+    alphabet.push(String.fromCharCode(i));
+	}
+
+	answer = alphabet.filter(item => !str.includes(item));
+
+	return answer.join('')
+}
+
+console.log(solution('His comments came after Pyongyang announced it had a plan to fire four missiles near the US territory of Guam.')) // solved
