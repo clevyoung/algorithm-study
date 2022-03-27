f8c429698ec6a8255e9d674e681813fd8ebae349
김응길 <yesroad.dev@gmail.com>
2022-03-27 15:02:50 +0900

    배열의회전 - solved

---
+++ programmers/test/test02/eunggil.js
@@ -0,0 +1,17 @@
+function solution(arrA, arrB) {
+  let answer = false;
+  let result = arrB.join('') 
+
+  for(let i=1; i<=arrA.length; i++){
+    const arr = arrA
+    const pop = arrA.pop();
+    arr.unshift(pop)
+    if(result === arr.join('')){
+      answer = true
+    }
+  }
+  return answer
+}
+
+solution([1,2,3,4,5], [3,4,5,1,2]) // solved
+
